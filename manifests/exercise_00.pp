c3d926cb4f817e49d055342c0e84ccf1ecb828f1
Mark Veltzer <mark.veltzer@gmail.com>
2020-10-18 07:45:34 +0300

    an exercise

---
+++ manifests/exercise_00.pp
@@ -0,0 +1,9 @@
+node default {
+	file { '/etc/puppet_exercise_00': #the path of the new file
+		ensure => 'present',
+		content => 'this is the content', #this text will be inside the file
+		owner => 'root',
+		group => 'root',
+		mode => '0644',
+	}
+}
